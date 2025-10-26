6a98bb168f39019f87c225cd69f624988f7e1c8a
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-10-26 10:41:02 +0100

    ultmos

--- app/env-check.tsx
+++ app/env-check.tsx
@@ -1,8 +1,8 @@
 export default function EnvCheck() {
   return (
-    <div>
-      <p>URL: {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
-      <p>
+    <div data-oid="once.x6">
+      <p data-oid="a6au_2.">URL: {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
+      <p data-oid="t9z0v.2">
         Anon Key exists:{" "}
         {process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "YES" : "NO"}
       </p>
